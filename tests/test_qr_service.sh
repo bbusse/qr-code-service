cde83824f0860edd63f6def2b2a5f4234422f461
Björn Busse <bj.rn@baerlin.eu>
2021-04-15 22:36:55 +0200

    tests: Change dir for tests to run properly

--- tests/test_qr_service.sh
+++ tests/test_qr_service.sh
@@ -6,7 +6,8 @@ setup_suite() {
 
 run_service() {
     printf "Starting qr-service\n"
-    ../qr_service > /dev/null 2>&1 &
+    cd ../ || exit
+    ./qr_service > /dev/null 2>&1 &
     PID=$!
     sleep 5
 }
@@ -16,13 +17,13 @@ test_qr_service_running() {
 }
 
 test_qr_encode() {
-    png=$(curl -s http://localhost:44123/encode?url=http://github.com/bbusse/qr-service&size=15&margin=2)
+    png=$(curl -s "http://localhost:44123/encode?url=http://github.com/bbusse/qr-service&size=15&margin=2")
     md5sum=$(echo "$png" | md5)
     assert_equals "824dd62f5c5e2b1da68b1853712bb9e5" "$md5sum" "qr: Receiving qr-code file failed"
 }
 
 test_qr_list() {
-    $(touch ../static/0a52ee86bcd81feccf8641735563ac3f_15_2.png)
+    $(touch static/0a52ee86bcd81feccf8641735563ac3f_15_2.png)
     r=$(curl -s http://localhost:44123/files)
     assert_equals "[\"0a52ee86bcd81feccf8641735563ac3f_15_2.png\"]" "$r" "qr: File listing failed"
 }
